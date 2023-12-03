e2d78af9453ac752efe9f6ce83f78203fcdc2857
ks0927 <brian_als@naver.com>
2023-12-03 15:46:29 +0900

    feat: 게임 플레이 결정 값을 입력 받는 메서드 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,14 +1,21 @@
 package baseball.view;
 
 import baseball.domain.BaseballNumbers;
+import baseball.domain.GameDecision;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String READ_BASEBALL_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String READ_GAME_DECISION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private final InputMapper inputMapper = new InputMapper();
 
     public BaseballNumbers readBaseballNumbers() {
         System.out.print(READ_BASEBALL_NUMBER_MESSAGE);
         return inputMapper.mapToBaseballNumbers(Console.readLine());
+    }
+
+    public GameDecision readGameDecision() {
+        System.out.print(READ_GAME_DECISION_MESSAGE);
+        return inputMapper.mapToGameDecision(Console.readLine());
     }
 }
