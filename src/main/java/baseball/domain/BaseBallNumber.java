186486f5486076fc73fae39bbd5a2e91fd9b0075
ks0927 <brian_als@naver.com>
2023-12-03 13:26:34 +0900

    feat: BaseballNumber에 equals와 hascode 추가

--- src/main/java/baseball/domain/BaseBallNumber.java
+++ src/main/java/baseball/domain/BaseBallNumber.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import java.util.Objects;
+
 public class BaseballNumber {
     private static final int MIN_RANGE = 1;
     private static final int MAX_RANGE = 9;
@@ -19,5 +21,22 @@ public class BaseballNumber {
 
     private boolean isInvalidRange(int target) {
         return MIN_RANGE > target || target > MAX_RANGE;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BaseballNumber that = (BaseballNumber) o;
+        return ballNumber == that.ballNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ballNumber);
     }
 }
